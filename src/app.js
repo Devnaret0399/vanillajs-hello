c436767371a5922a886b2ff81842b18c24780d3f
Pablo Teran <144842209+Devnaret0399@users.noreply.github.com>
2024-10-03 03:51:48 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ['el', 'la','nuestro'];
+let adj = ['mejor', 'gran','espectacular'];
+let noun = ['pagina', 'web','dominio'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+for(let i = 0; i < pronoun.length; i++) {
+    for(let j = 0; j < adj.length; j++) {
+      for(let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k]+'.com'); 
+      }
+    }
+  }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  
